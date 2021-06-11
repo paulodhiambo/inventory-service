739e66e0830c204d7e5539bc108ea1a16a683740
Paul Odhiamo <paulodhiambo962@gmail.com>
2021-06-11 18:53:06 +0300

    added jms config

--- target/generated-sources/annotations/com/odhiambopaul/beer/inventory/service/web/mappers/BeerInventoryMapperImpl.java
+++ target/generated-sources/annotations/com/odhiambopaul/beer/inventory/service/web/mappers/BeerInventoryMapperImpl.java
@@ -10,8 +10,8 @@ import org.springframework.stereotype.Component;
 
 @Generated(
     value = "org.mapstruct.ap.MappingProcessor",
-    date = "2021-01-01T20:05:30+0300",
-    comments = "version: 1.3.0.Final, compiler: javac, environment: Java 15.0.1 (Oracle Corporation)"
+    date = "2021-05-24T21:40:48+0300",
+    comments = "version: 1.3.0.Final, compiler: javac, environment: Java 15.0.2 (Oracle Corporation)"
 )
 @Component
 public class BeerInventoryMapperImpl implements BeerInventoryMapper {
